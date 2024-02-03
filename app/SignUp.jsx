f2dfcd459b9889d88661282cf7110659f6678b72
William Horn <williamjosephhorn@gmail.com>
2024-02-02 19:05:55 -0500

    initial

---
+++ app/SignUp.jsx
@@ -0,0 +1,84 @@
+
+"use client"
+
+import Text from "@/components/Typography/Text";
+
+import stringIsEmpty from "@/libs/utils/stringIsEmpty";
+
+import { useState, useEffect, useRef } from "react";
+import { StatelessButton } from "@/components/Buttons/Buttons";
+import { useLocalStorageState } from "@/hooks/useLocalStorageRequest";
+
+const SignUp = function({
+  countEmails,
+  postEmail
+}) {
+  const [getEmailHistory, setEmailHistory] = useLocalStorageState("nilhemoth:email", "not used");
+  const [emailCount, setEmailCount] = useState(null);
+  const [usedEmail, setUsedEmail] = useState(false);
+  const inputRef = useRef(null);
+
+  const onEmailPost = (e) => {
+    console.log('fired onsubmit');
+    // e.preventDefault();
+    
+    const text = inputRef.current.value;
+    if (stringIsEmpty(text) || getEmailHistory() === "used") return;
+
+    console.log('submitted', text);
+    
+    setEmailHistory(() => ("used"));
+    setUsedEmail(true);
+
+    postEmail(text)
+      .then(res => {
+        console.log('created: ', res);
+      });
+  }
+
+  const updateEmailCount = () => {
+    countEmails()
+      .then((count) => {
+        setEmailCount(count);
+      });
+  }
+
+  useEffect(() => {
+    updateEmailCount();
+    setUsedEmail(getEmailHistory());
+  }, []);
+
+  return (
+    <>
+      <form className="flex xl:w-[30%] lg:w-[35%] md:w-[40%] sm:w-[50%] w-[90%] max-w-[400px] mb-1" onSubmit={onEmailPost}>
+        <div className="flex bg-transparent w-full">
+          <input
+          ref={inputRef}
+          autoComplete="email"
+          placeholder="name@example.com"
+          type="email"
+          className="w-full text-black font-gf-2 p-1"
+          />
+        </div>
+        <StatelessButton
+        className={{
+          self: "rounded-none font-bold bg-[#5e5ecf] hover:bg-[#7777df] py-1 px-2"
+        }}
+        type="submit"
+        >
+          Submit
+        </StatelessButton>
+      </form>
+
+      {
+        usedEmail === "used"
+          ? <Text textSize="md" className="text-center text-green-500 mb-8">Your email has been submitted!</Text>    
+          : <div className="mb-8"></div>
+      }
+
+      <Text textSize="md" className="font-special text-yellow-500 mb-10">JOIN <b>{emailCount}</b> OTHER PEOPLE!</Text>
+    </>
+  );
+}
+
+export default SignUp;
---
+++ app/page.js
@@ -0,0 +1,77 @@
+
+
+import Page from "@/components/Page";
+import Image from "next/image";
+import Content from "@/components/Content";
+import Text from "@/components/Typography/Text";
+import { StatelessButton } from "@/components/Buttons/Buttons";
+
+import connectMongoDB from "@/libs/db/mongodb";
+import Email from "@/models/email/email";
+
+import SignUp from "./SignUp";
+
+const postEmail = async (email) => {
+  "use server";
+
+  await connectMongoDB('production');
+
+  const original = await Email.create({
+    email
+  });
+
+  console.log(original);
+
+  return toSimpleArray(original);
+}
+
+const getEmailCount = async () => {
+  "use server";
+
+  await connectMongoDB('production');
+
+  const count = await Email.countDocuments();
+
+  return count;
+}
+
+const HomePage = function() {
+  return (
+    <Page className="bg-[#0c0c0c] min-h-screen">
+      <Content span="max">
+
+        <Content span="md" className="mx-auto  flex flex-col items-center pt-3">
+          <div className="relative w-[896px] h-[500px] mb-8 max-w-[100%]">
+            <Image
+            fill
+            alt=""
+            src="/images/banner-img.jpg"
+            sizes="1200px"
+            />
+          </div>
+
+          <Text textSize="2xl" className="font-special text-yellow-500 mb-2">5 ACTS: THE KING IN YELLOW RPG ... AND BEYOND!</Text>
+          <Text textSize="xl" className="font-special text-yellow-500 mb-5">BY NILHEMOTH GAMES</Text>
+          <Text textSize="2xl" className="font-special text-yellow-500 mb-10">FOLLOW THE GAME FOR EXCLUSIVE BONUS CONTENT!</Text>
+
+          <Text textSize="md" className="font-special text-yellow-500 mb-3 font-bold">GET PROJECT NEWS AND UPDATES BY PROVIDING YOUR EMAIL</Text>
+
+          <SignUp postEmail={postEmail} countEmails={getEmailCount}/>
+
+          <div className="relative w-[896px] h-[500px] mb-8 max-w-[100%]">
+            <Image
+            fill
+            alt=""
+            src="/images/bg-art-1.jpg"
+            sizes="1200px"
+            />
+          </div>
+
+        </Content>
+
+      </Content>
+    </Page>
+  );
+}
+
+export default HomePage;
